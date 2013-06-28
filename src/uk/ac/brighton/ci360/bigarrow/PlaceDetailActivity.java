fbc6aa0784a360b1f1736c84fad401d7a89764a0
AlmasB <toll.everon.com>
2013-06-29 00:42:01 +0100

    replaced way of generating expandable view from hardcoded to dynamic

--- src/uk/ac/brighton/ci360/bigarrow/PlaceDetailActivity.java
+++ src/uk/ac/brighton/ci360/bigarrow/PlaceDetailActivity.java
@@ -1,6 +1,7 @@
 package uk.ac.brighton.ci360.bigarrow;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 import uk.ac.brighton.ci360.bigarrow.places.Place;
 import uk.ac.brighton.ci360.bigarrow.places.PlaceDetails;
@@ -24,7 +25,6 @@ public class PlaceDetailActivity extends PlaceSearchActivity {
 	private ExpandListAdapter ExpAdapter;
 	private ExpandableListView ExpandList;
 	
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		firstSearchType = SearchType.DETAIL;
@@ -39,11 +39,6 @@ public class PlaceDetailActivity extends PlaceSearchActivity {
 		pDialog.setIndeterminate(false);
 		pDialog.setCancelable(false);
 		pDialog.show();
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) { 
-		// TODO Auto-generated method stub
 	}
 
 	@Override
@@ -74,16 +69,16 @@ public class PlaceDetailActivity extends PlaceSearchActivity {
 			{
 				ArrayList<ExpandListGroup> list = new ArrayList<ExpandListGroup>();
 		    	ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
-				
-				String[] groupNames = {"Name", "Address", "Phone", "Location", "Rating", "Opening Hours"};
-				String[] childNames = getFormattedPlaceDetails(place);
-				
-				for (int i = 0; i < groupNames.length; i++)
-				{
-					ExpandListGroup group = new ExpandListGroup();
-			        group.setName(groupNames[i]);
+		    	
+		    	final LinkedHashMap<String, String> details = place.getDetails();
+		    	
+		    	//loop through all details available in the hashmap
+		    	for (String key : details.keySet())
+		    	{
+		    		ExpandListGroup group = new ExpandListGroup();
+			        group.setName(key);
 			        ExpandListChild child = new ExpandListChild();
-			        child.setName(childNames[i]);
+			        child.setName(details.get(key));
 			        child.setTag(null);
 			        list2.add(child);
 	
@@ -91,37 +86,16 @@ public class PlaceDetailActivity extends PlaceSearchActivity {
 			        list2 = new ArrayList<ExpandListChild>();
 			        
 			        list.add(group);
-				}
+		    	}
 				
 				return list;
 			}
-			
-			/**
-			 * @param place - the place, details of which are asked
-			 * @return - a String array containing correctly formatted (no nulls)
-			 * details of the place in order
-			 */
-			private String[] getFormattedPlaceDetails(Place place)
-			{
-				String[] details = new String[6];
-				
-				//I have no choice at the moment
-				//However I plan to replace it with enums and dynamic code
-				details[0] = format(place.name);
-				details[1] = format(place.formatted_address);
-				details[2] = format(place.formatted_phone_number);
-				details[3] = "Latitude: " + format(Double.toString(place.getLatLng().latitude))
-						+ " Longitude: " + format(Double.toString(place.getLatLng().longitude));
-				details[4] = format(Double.toString(place.rating));
-				details[5] = place.opening_hours == null ? "Not present" : (place.opening_hours.open_now ? "open now" : "closed");
-				
-				return details;
-			}
-			
-			private String format(String detail) {
-				return detail == null ? "Not present" : detail;
-			}
 		});
+	}
+	
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) { 
+		// TODO Auto-generated method stub
 	}
 
 	@Override
