1235599c62216026bc833b2eef7ea7d982cb79c9
AlmasB <toll.everon.com>
2013-06-30 01:37:46 +0100

    added 2 helper methods

--- src/uk/ac/brighton/ci360/bigarrow/classes/Utils.java
+++ src/uk/ac/brighton/ci360/bigarrow/classes/Utils.java
@@ -1,6 +1,10 @@
 package uk.ac.brighton.ci360.bigarrow.classes;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import android.annotation.SuppressLint;
+import android.location.Location;
+import android.location.LocationManager;
 import uk.ac.brighton.ci360.bigarrow.places.Place.Detail;
 
 public class Utils
@@ -18,5 +22,39 @@ public class Utils
 	@SuppressLint("DefaultLocale")
 	public static String format(Detail detail) {
 		return detail.name().toLowerCase().replace('_', ' ');
+	}
+	
+	/**
+	 * Returns the current location of the requester
+	 * using best available provider
+	 * May return null if no providers available or
+	 * providers cannot be used under current conditions
+	 * Checking result for null is compulsory
+	 * @param lm - location manager
+	 * @return current location
+	 */
+	public static Location getMyLocation(LocationManager lm) {
+		if (lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+			return lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		
+		return lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	}
+
+	/**
+	 * @param ll1 - first pair of coords
+	 * @param ll2 - second pair of coors
+	 * @return Approximate distance in meters between 2 pairs of lat
+	 * and lng coordinates
+	 */
+	public static float distanceBetween(LatLng ll1, LatLng ll2) {
+		Location loc1 = new Location("");
+		loc1.setLatitude(ll1.latitude);
+		loc1.setLongitude(ll1.longitude);
+		
+		Location loc2 = new Location("");
+		loc2.setLatitude(ll2.latitude);
+		loc2.setLongitude(ll2.longitude);
+		
+		return loc1.distanceTo(loc2);
 	}
 }
